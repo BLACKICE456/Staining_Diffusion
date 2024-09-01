dbdb42e210045cd5b3dbd18266b5f4d38abf0c00
Xiaofeng Deng <108693800+BLACKICE456@users.noreply.github.com>
2024-09-01 13:17:15 +0200

    Add files via upload

--- train.py
+++ train.py
@@ -7,24 +7,36 @@ from src.residual_denoising_diffusion_pytorch import (ResidualDiffusion,
                                                       set_seed)
 
 # init
-os.environ['CUDA_VISIBLE_DEVICES'] = ','.join(str(e) for e in [0])
+#os.environ['CUDA_VISIBLE_DEVICES'] = ','.join(str(e) for e in [0,1,2,3])
+os.environ['NCCL_IB_TIMEOUT'] = '22'
+os.environ['NCCL_IB_GID_INDEX'] = '3'
 sys.stdout.flush()
 set_seed(10)
 debug = False
 
+
+
 if debug:
     save_and_sample_every = 2
-    sampling_timesteps = 10
+    sampling_timesteps = 2
     sampling_timesteps_original_ddim_ddpm = 10
-    train_num_steps = 200
+    train_num_steps = 20
+    resume = False
 else:
-    save_and_sample_every = 1000
+    save_and_sample_every = 10000
+    """
     if len(sys.argv) > 1:
+        print(sys.argv)
         sampling_timesteps = int(sys.argv[1])
     else:
-        sampling_timesteps = 10
+    """
+    sampling_timesteps = 10 # (50,)
     sampling_timesteps_original_ddim_ddpm = 250
-    train_num_steps = 100000
+    train_num_steps = 100000 #100000
+    resume = False
+    parallel_test_bool = True
+
+    #define epochs
 '''
 original_ddim_ddpm = False
 if original_ddim_ddpm:
@@ -42,29 +54,17 @@ input_condition = False
 input_condition_mask = False
 if condition:
     # Image restoration  
-    if input_condition:
-        folder = ["xxx/dataset/ISTD_Dataset_arg/data_val/ISTD_shadow_free_train.flist",
-                  "xxx/dataset/ISTD_Dataset_arg/data_val/ISTD_shadow_train.flist",
-                  "xxx/dataset/ISTD_Dataset_arg/data_val/ISTD_mask_train.flist",
-                  "xxx/dataset/ISTD_Dataset_arg/data_val/ISTD_shadow_free_test.flist",
-                  "xxx/dataset/ISTD_Dataset_arg/data_val/ISTD_shadow_test.flist",
-                  "xxx/dataset/ISTD_Dataset_arg/data_val/ISTD_mask_test.flist"]
-    else:
-        folder = [r"D:\paper_model\BCI\BCI_dataset\HE\train",
-                  r"D:\paper_model\BCI\BCI_dataset\IHC\train",
-                  r"D:\paper_model\BCI\BCI_dataset\HE\test",
-                  r"D:\paper_model\BCI\BCI_dataset\IHC\test"]
+
+
+    folder = [r"/mnt/data/BCI/train/IHC",
+              r"/mnt/data/BCI/train/HE",
+              r"/mnt/data/BCI/test/IHC",
+              r"/mnt/data/BCI/test/HE"]
+
     train_batch_size = 2
-    num_samples = 4
-    sum_scale = 0.01
-    image_size = 256
-else:
-    # Image Generation 
-    folder = 'xxx/CelebA/img_align_celeba'
-    train_batch_size = 128
-    num_samples = 64
-    sum_scale = 1
-    image_size = 64
+    num_samples = 2
+    sum_scale = 0.1
+    image_size = 512 #check the consume of image and model
 
 num_unet = 2
 objective = 'pred_res_noise'
@@ -101,6 +101,7 @@ else:
         loss_type='l2',            # L1 or L2
         condition=condition,
         sum_scale=sum_scale,
+        # scale of noise
         input_condition=input_condition,
         input_condition_mask=input_condition_mask,
         test_res_or_noise = test_res_or_noise
@@ -120,9 +121,12 @@ trainer = Trainer(
     condition=condition,
     save_and_sample_every=save_and_sample_every,
     equalizeHist=False,
-    crop_patch=False,
+    crop_patch=True,
     generation=True,
     num_unet=num_unet,
+    resume_load = resume,
+    parallel_test = parallel_test_bool,
+    sum_scale_train=sum_scale
 )
 
 # train
@@ -130,20 +134,40 @@ trainer = Trainer(
 if __name__ == '__main__':
 
     freeze_support()
-    
+
+    if resume and trainer.accelerator.is_local_main_process:
+        trainer.load(4)
+
 
     
+    if not parallel_test_bool:
+        trainer.train()
 
-    trainer.train()
+    if parallel_test_bool:
+        for i in range(4,5):
+
+            trainer.load(i)
+            """
+            trainer.set_results_folder(
+                '/mnt/data/result_ge47nej/test_timestep_' + str(sampling_timesteps) + "_" + str(train_num_steps) + "num_scale_" + str(sum_scale))
+            trainer.set_results_folder_sample(
+                '/mnt/data/result_ge47nej/test_timestep_sample_' + str(sampling_timesteps) + " " + str(train_num_steps) + "num_scale_" + str(sum_scale))
+            """
+            trainer.parallel_test_sample(last=True,ckpt_num=i)
 
     # test
+    """
     if not trainer.accelerator.is_local_main_process:
         pass
     else:
         trainer.load(trainer.train_num_steps//save_and_sample_every)
         trainer.set_results_folder(
-            './results/test_timestep_'+str(sampling_timesteps))
-        trainer.test(last=True)
+            './results/test_timestep_'+str(sampling_timesteps) + str(train_num_steps))
+        trainer.set_results_folder_sample(
+            './results/test_timestep_sample_' + str(sampling_timesteps) + str(train_num_steps))
+
+        #trainer.test(last=True)
+    """
 
 # trainer.set_results_folder('./results/test_sample')
 # trainer.test(sample=True)
